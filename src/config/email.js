1085235c3e8f683925396f0cff661f6ffc123123
evreneducation <65123232+evreneducation@users.noreply.github.com>
2025-08-26 17:58:24 +0530

    Update email.js

--- src/config/email.js
+++ src/config/email.js
@@ -6,8 +6,8 @@ config();
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
@@ -39,4 +39,5 @@ transporter
     console.log("Email verification error: ", error);
     process.exit(1);
   });
+
 
